7307e3b291281a3fb46b2751e304d0e5494d4011
Rodrigo <rodrigo19962010@live.com>
2017-04-26 09:14:36 -0300

    More changes

--- receive_logs_topic.rb
+++ receive_logs_topic.rb
@@ -1,9 +1,8 @@
-
-
 #!/usr/bin/env ruby
 # encoding: utf-8
 
 require "bunny"
+require 'pp'
 
 if ARGV.empty?
   abort "Usage: #{$0} [binding key]"
@@ -16,20 +15,44 @@ ch  = conn.create_channel
 x   = ch.topic("topic_logs")
 q   = ch.queue("", :exclusive => true)
 
+isArray=0
+
 ARGV.each do |severity|
+  
   q.bind(x, :routing_key => severity)
+
+  if severity.include? " "
+
+  	isArray = 1
+
+  	array = Array.new
+
+  	array = severity.split(/ /)
+
+  	array.size.times do |item|
+  		puts array[item]
+  	end
+
+  end
+
 end
 
-puts " [*] Waiting for logs. To exit press CTRL+C"
+puts "[*] Waiting for logs. To exit press CTRL+C"
 
 begin
+
+  i = 1
   
   ARGV.each do |severity|
   	
-  	q.subscribe(:block => true) do |delivery_info, properties, body|
-  	if severity=="#" or severity==delivery_info.routing_key
-    	puts " [x] #{delivery_info.routing_key}:#{body}"
-  	end
+  q.subscribe(:block => true) do |delivery_info, properties, body|
+
+	      if severity=="#" or severity==delivery_info.routing_key
+	        	puts " [#{i}] - #{body}"
+	      end
+
+      	  i = i+1
+
   end
   
 end

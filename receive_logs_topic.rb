f35adb392fe410bb9d3d8f721997c28c8e20ae3b
josafa <desenvolvedorjosafa@gmail.com>
2017-04-27 08:15:06 -0300

    Remove unusual things

--- receive_logs_topic.rb
+++ receive_logs_topic.rb
@@ -2,7 +2,6 @@
 # encoding: utf-8
 
 require "bunny"
-require 'pp'
 
 if ARGV.empty?
   abort "Usage: #{$0} [binding key]"
@@ -20,20 +19,6 @@ isArray=0
 ARGV.each do |severity|
   
   q.bind(x, :routing_key => severity)
-
-  if severity.include? " "
-
-  	isArray = 1
-
-  	array = Array.new
-
-  	array = severity.split(/ /)
-
-  	array.size.times do |item|
-  		puts array[item]
-  	end
-
-  end
 
 end
 
